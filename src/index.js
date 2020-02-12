cb2599c28a3149b9f122b1332d45427d54451b49
Colin Gourlay <gourlay.colin@abc.net.au>
2020-02-12 15:06:09 +1000

    More work on mini-maps, including a new SVG-based one for the opening panny

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ function initColdOpen() {
   const headerEl = document.querySelector('.Header');
   const headerContentEl = headerEl.querySelector('.Header-content');
   const coldOpenBlockEl = document.querySelector('.Block');
+  const coldOpenBlockMediaEl = coldOpenBlockEl.querySelector('.Block-media');
   const coldOpenHeaderEl = coldOpenBlockEl.querySelector('.Block-content').cloneNode(true);
+
+  coldOpenBlockMediaEl.classList.add(styles.coldOpenMedia);
 
   coldOpenHeaderEl.innerHTML = '';
   coldOpenHeaderEl.classList.add('Header-content', styles.coldOpenHeader);
@@ -27,15 +30,18 @@ function initBlockMediaPosition() {
 }
 
 function initMiniMapMargin() {
-  [...document.querySelectorAll('.ImageEmbed.u-pull-right')].forEach(el => {
-    el.classList.add(styles.miniMap);
-  });
+  [...document.querySelectorAll('.ImageEmbed')]
+    .filter(el => el.querySelector('.Sizer.lg-1x1'))
+    .forEach(el => {
+      el.classList.add(styles.miniMap);
+    });
 }
 
 function init() {
   initColdOpen();
   initBlockMediaPosition();
   initMiniMapMargin();
+  window.__ODYSSEY__.scheduler.invalidateClient();
 }
 
 window.__ODYSSEY__ ? init() : window.addEventListener('odyssey:api', init);
